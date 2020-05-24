2a8ed6ffc7237e90621e6c0c676d7b4043eee000
Dipan Nama <dipannama468@gmail.com>
2020-05-24 08:47:40 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+import requests
+import threading
+#url = input('[+]Enter website url: ')
+base_url = 'https://www.w3schools.com/'
+#dir = ['maakiankh','html','css','js','javascript','py','python','asp','json','php','c','c++','cpp']
+
+class main:
+	def find_dir(self):
+		f = open('crawl.txt')
+		f.readlines()
+		for line in f:
+			i = line[:-1]
+			url = f'{base_url}/{i}'
+			r = requests.get(url=url)
+			s = int(r.status_code)
+			if s == 200:
+				print(f'Found: /{i}/')
+			else:
+				print(f'not Fonud: {i}')
+
+myobj = main()
+t1 = threading.Thread(target=myobj.find_dir)
+t1.start()
+t1.join()
+print('the end')
